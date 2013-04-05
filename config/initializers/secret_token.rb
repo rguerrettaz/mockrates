fedd6531108189643c7ed6251eef4c261806c4c5
dbc gogo <dbc@dbc.com>
2013-04-05 10:50:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mockrates::Application.config.secret_token = '5fbfb4f24df0fc50ca981ede1c296d4f70ed6775f941f8a48c6eca239ee0979e9b82d8bf746a54a956638985c45458f44f32e736f568f9a05545c791f8c33db0'
