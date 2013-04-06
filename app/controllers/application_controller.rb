ad695d302020a92b9b5354bab290756cdb27538b
Apprentice <apprentice@devbootcamp.com>
2013-04-05 19:38:59 -0700

    double login v1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,30 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
  def sign_in(user)
-    cookies[:id] = user.id
+    if cookies[:id]
+      cookies[:id2] = user.id
+    else
+      cookies[:id] = user.id
+    end
+  end
+
+  def second_current_user
+    @second_current_user ||= User.find(cookies[:id2]) if cookies[:id2]
   end
 
   def current_user
     @current_user ||= User.find(cookies[:id]) if cookies[:id]
   end
 
+  def pair_users?
+    second_current_user && current_user
+  end
+  def active_user
+    @active_user = current_user || second_current_user
+  end
 
 
-  helper_method :current_user
+
+  helper_method :current_user, :second_current_user, :pair_users?, :active_user
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
-
+  require 'debugger'
   def new
   end
 
   def create
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
+      return redirect_to root_path, :notice => "You have logged in" if @user == active_user
       sign_in(@user)
       redirect_to @user
     else
@@ -23,7 +24,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    cookies.delete(:id)
+    cookies.delete(:id) if params[:id] == cookies[:id]
+    cookies.delete(:id2) if params[:id] == cookies[:id2]
     redirect_to root_url
   end
 
