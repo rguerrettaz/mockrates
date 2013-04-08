f86c53d41a4e20d881a8368eaf6db923ec069311
dbc gogo <dbc@dbc.com>
2013-04-07 17:05:33 -0700

    fixed database issue in interactive attempts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406234910) do
+ActiveRecord::Schema.define(:version => 20130407235733) do
 
   create_table "attempts", :force => true do |t|
     t.integer  "user_id"
